e27df8068c278a0438e32d8cdacb43869f35320c
rahulbhadja <rahulbhadja@gmail.com>
2021-11-22 00:13:07 +0530

    crated basic front-end for lottery project in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import web3 from './web3';
+import lottery from './lottery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+  };
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.manager &&
+            `This contract is managed by ${this.state.manager.slice(
+              0,
+              4
+            )}...${this.state.manager.slice(-4)}`}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h4>
+          There are currently {this.state.players.length} people
+          enterd,competing to win{' '}
+          {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </h4>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/lottery.js
@@ -0,0 +1,59 @@
+import web3 from './web3';
+
+const address = '0x89054A82162a4d4e4eAaE323EA1FE128618BcBb5';
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    payable: true,
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
